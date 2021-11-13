eb50908402b769fda6a8b40718ac460245739f0f
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2021-11-13 22:13:38 +0530

    Added mocha reporter to generate HTML report file

---
+++ cypress/scripts/cypress.js
@@ -0,0 +1,23 @@
+const cypress = require('cypress')
+const { merge } = require('mochawesome-merge')
+const fse = require('fs-extra')
+const generator = require('mochawesome-report-generator')
+
+const options = {
+    reportDir: 'TestReports',
+    files: ["TestReports/*.json"],
+    showPending: false
+}
+
+async function runTests() {
+    await fse.remove('TestReports');
+    const testRun = await cypress.run({
+        browser: 'chrome'
+    })
+    const totalFailed = testRun['totalFailed']
+    const jsonReport = await merge(options)
+    await generator.create(jsonReport, options);
+    process.exit(totalFailed);
+}
+
+runTests()
