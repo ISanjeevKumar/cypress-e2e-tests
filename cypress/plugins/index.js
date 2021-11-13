39e1125ff08d332f66d5e977d2d24e776fa1d873
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2021-11-13 17:23:53 +0530

    Added Mechanism to Handle Environment Configs

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,11 +12,31 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const getEnvironmnetUrls = require("../config/runConfig")
+
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+
+const {
+  ENVIRONMENT,
+  TEST
+} = process.env
+
+const getRunConfigs = function (cypressConfig) {
+
+  const envBasedRunCypressConfig = getEnvironmnetUrls(ENVIRONMENT);
+  envBasedRunCypressConfig.integrationFolder = `./cypress/integration/${TEST}`;
+  
+  envBasedRunCypressConfig.env = Object.assign(cypressConfig.env, envBasedRunCypressConfig.env);
+  const config = Object.assign(cypressConfig, envBasedRunCypressConfig);
+
+  return config;
 }
+
+  // eslint-disable-next-line no-unused-vars
+  module.exports = (on, config) => {
+    // `on` is used to hook into various events Cypress emits
+    // `config` is the resolved Cypress config
+    return getRunConfigs(config);
+  }
