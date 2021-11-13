915fbc8877409aba9402f3f42ec23c31205d6b22
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2021-11-13 19:51:32 +0530

    Added Login Tests with PageObjects design pattern

---
+++ cypress/pageObjects/HomePage.js
@@ -0,0 +1,36 @@
+/// <reference types="Cypress"/>
+
+const locators = {
+    DISMISS_COOKIES_BTN: 'a.cc-btn.cc-dismiss',
+    WELCOME_BANNER_BTN: '.close-dialog',
+    ACCOUNT_LINK: '#navbarAccount',
+    LOGIN_BTN: '#navbarLoginButton'
+}
+
+function visit(applicationUrl) {
+    cy.visit(applicationUrl).then(function () {
+        dismissCooKies()
+        closeWelcomeBanner()
+    })
+    console.log(`Navigated to ${applicationUrl}`)
+}
+
+function navigateToLoginPage() {
+    cy.get(locators.ACCOUNT_LINK).should('be.visible').click()
+    cy.get(locators.LOGIN_BTN).should('be.visible').click()
+}
+
+function dismissCooKies() {
+    cy.get(locators.DISMISS_COOKIES_BTN).should('be.visible').click()
+}
+
+function closeWelcomeBanner() {
+    cy.get(locators.WELCOME_BANNER_BTN).should('be.visible').click()
+}
+
+module.exports = {
+    visit,
+    navigateToLoginPage,
+    dismissCooKies,
+    closeWelcomeBanner
+}
