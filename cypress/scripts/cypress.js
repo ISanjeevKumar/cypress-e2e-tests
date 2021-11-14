0ff1293dd00609d0dd4a1869981c93973cf717cb
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2021-11-14 16:59:11 +0530

    Added mechanism to pass browser name from CLI

--- cypress/scripts/cypress.js
+++ cypress/scripts/cypress.js
@@ -2,17 +2,27 @@ const cypress = require('cypress')
 const { merge } = require('mochawesome-merge')
 const fse = require('fs-extra')
 const generator = require('mochawesome-report-generator')
+const yargs = require('yargs')
 
 const options = {
     reportDir: 'TestReports',
     files: ["TestReports/*.json"],
     showPending: false
 }
+const argv = yargs.options({
+    'browser': {
+        alias: 'b',
+        describe: 'choose browser that you wanna run tests on',
+        default: 'chrome',
+        choices: ['chrome', 'electron', 'firefox', 'edge']
+    }
+}).help()
+    .argv
 
 async function runTests() {
     await fse.remove('TestReports');
     const testRun = await cypress.run({
-        browser: 'chrome'
+        browser: argv.browser
     })
     const totalFailed = testRun['totalFailed']
     const jsonReport = await merge(options)
