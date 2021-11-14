aef4d9aee20b32279c0b1598de4bdf15c61dab16
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2021-11-14 10:53:38 +0530

    Updated package.json file and Added CustomerFeedback tests

---
+++ cypress/integration/smoke/CustomerFeedback.js
@@ -0,0 +1,14 @@
+import { HomePage } from '../../pageObjects/App'
+
+describe('Customer feedbacks', function () {
+
+    this.beforeEach('Navigate to application', function () {
+        HomePage.visit(Cypress.env('baseUrl'))
+    })
+
+    it('customer should be able to provide feedback', function () {
+        HomePage.openSideMenu()
+        HomePage.navigateToCustomerFeedbackPage()
+    })
+
+})
--- cypress/pageObjects/HomePage.js
+++ cypress/pageObjects/HomePage.js
@@ -4,7 +4,9 @@ const locators = {
     DISMISS_COOKIES_BTN: 'a.cc-btn.cc-dismiss',
     WELCOME_BANNER_BTN: '.close-dialog',
     ACCOUNT_LINK: '#navbarAccount',
-    LOGIN_BTN: '#navbarLoginButton'
+    LOGIN_BTN: '#navbarLoginButton',
+    OPEN_SIDENAV_BTN: 'button[aria-label="Open Sidenav"]',
+    CUSTOMER_FEEDBACK_LINK:'[routerlink="/contact"] > .mat-list-item-content > .menu-text'
 }
 
 function visit(applicationUrl) {
@@ -20,6 +22,10 @@ function navigateToLoginPage() {
     cy.get(locators.LOGIN_BTN).should('be.visible').click()
 }
 
+function navigateToCustomerFeedbackPage() {
+    cy.get(locators.CUSTOMER_FEEDBACK_LINK).should('be.visible').click()
+}
+
 function dismissCooKies() {
     cy.get(locators.DISMISS_COOKIES_BTN).should('be.visible').click()
 }
@@ -28,9 +34,15 @@ function closeWelcomeBanner() {
     cy.get(locators.WELCOME_BANNER_BTN).should('be.visible').click()
 }
 
+function openSideMenu() {
+    cy.get(locators.OPEN_SIDENAV_BTN).should('be.visible').and('be.enabled').click()
+}
+
 module.exports = {
     visit,
     navigateToLoginPage,
     dismissCooKies,
-    closeWelcomeBanner
+    closeWelcomeBanner,
+    openSideMenu,
+    navigateToCustomerFeedbackPage
 }
