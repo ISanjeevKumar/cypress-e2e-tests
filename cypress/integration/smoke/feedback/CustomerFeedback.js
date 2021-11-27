20ea06366942eef3ef9de1dc981ba8201cb573af
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2021-11-27 15:10:42 +0530

    [Sanjeev] Fix Test scripts

--- cypress/integration/smoke/feedback/CustomerFeedback.js
+++ cypress/integration/smoke/feedback/CustomerFeedback.js
@@ -3,7 +3,7 @@ import { HomePage } from '../../../pageObjects/App'
 describe('Customer feedbacks', function () {
 
     this.beforeEach('Navigate to application', function () {
-        HomePage.visit(Cypress.env('baseUrl'))
+        HomePage.visit(Cypress.env('baseUrl'));
     })
 
     it('customer should be able to provide feedback', function () {
--- cypress/integration/smoke/login/LoginTest.js
+++ cypress/integration/smoke/login/LoginTest.js
@@ -1,10 +1,9 @@
 /// <reference types="Cypress"/>
-
 import { HomePage, LoginPage } from '../../../pageObjects/App'
 
 describe('Verify login functionality', function () {
 
-    before('Test initialization', function(){
+    before('Test initialization', function () {
         cy.fixture('login_testdata.json')
     })
 
@@ -12,17 +11,17 @@ describe('Verify login functionality', function () {
         HomePage.visit(Cypress.env('baseUrl'))
         cy.task('generateUserData').then((userCreds) => {
             this.user = userCreds;
-          })
+        })
 
     })
 
     this.afterEach(() => {
-       
-      })
+
+    })
 
     it('Verify user should not be able to login with invalid credentials', function () {
         HomePage.navigateToLoginPage()
-        LoginPage.login(this.user.username,this.user.password)
+        LoginPage.login(this.user.username, this.user.password)
         LoginPage.verifyErrorMessage('Invalid email or password.')
     })
 })
